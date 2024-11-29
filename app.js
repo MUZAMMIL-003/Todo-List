36b96c9ae3363460f8aae562d72f68d590d5bbcc
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2024-11-29 07:21:32 -0800

    Almost Completed...

--- app.js
+++ app.js
@@ -1,50 +1,75 @@
+/// All Variables are here..
+var todo = document.getElementById("todo")
 var list = document.getElementById("list")
-function addTodo() {
-    var todo = document.getElementById("todo")
 
-    // console.log(todo.value)
-    list.innerHTML +=` <li>
-    <input type = 'text' value ='${todo.value}' disabled /> 
-    <button onclick = 'editTodo(event)' type="button" class=" btn btn-outline-info"> Edit Todo</button> 
-    <button onclick = 'deleteTodo(event)' type="button" class="btn btn-outline-dark">Delete Todo </button>
-     </li>`;
-     todo.value = "";
-     for (let i = 0; i < todo.value.length; i++) {
-        localStorage.setItem("todoItems", JSON.stringify(todo.value[i]))
-        
-        
-    }
-    if (todo.value == " ") {
+/// To Add Todo tasks 
+function addTodo() {
+    if (todo.value == "") {
         alert("Fill The Inputs");
         return;
+    } else {
+        list.innerHTML += ` <li>
+        <input type = 'text' id = "inputValue" value ='${todo.value}' disabled /> 
+        <button onclick = 'editTodo(event)' type="button" class=" btn btn-outline-info"> Edit Todo</button> 
+        <button onclick = 'deleteTodo(event)' type="button" class="btn btn-outline-dark">Delete Todo </button>
+        </li>`;
     }
+    todo.value = "";
+    addData();
 }
+/// To add todo tasks by enter
 function addTodoByEnter(e) {
     if (e.keyCode === 13) {
         addTodo();
     }
-    
 }
+
+/// to delete todo tasks in localStorage too 
 function deleteTodo(e) {
-    e.target.parentNode.remove()
-    
+    e.target.parentNode.remove();
+    addData();
 }
+
+/// to edit todo tasks
 function editTodo(e) {
-var input =  e.target.parentNode.childNodes[1];
- input.disabled = false;
- input.focus();
- e.target.innerHTML = "Updated";
- e.target.setAttribute("onclick", 'updateTodo(event)')
-
+    var input = e.target.parentNode.childNodes[1];
+    input.disabled = false;
+    input.focus();
+    e.target.innerHTML = "Updated";
+    e.target.setAttribute("onclick", 'updateTodo(event)')
 }
+
+/// to update edit button
 function updateTodo(e) {
-    var input =  e.target.parentNode.childNodes[1];
- input.disabled = true;
- e.target.innerHTML = "Edited";
- input.focus();
-//  editTodo();
-
+    var input = e.target.parentNode.childNodes[1];
+    input.disabled = true;
+    e.target.innerHTML = "Edited";
+    input.focus();
 }
+
+
+/// to delete all todo tasks 
 function deleteAll(e) {
     list.innerHTML = "";
 }
+
+
+/// to add data in localStorage
+function addData() {
+    localStorage.setItem("data", list.innerHTML)
+}
+
+
+/// to remove data from localStorage
+function removeData() {
+    localStorage.removeItemItem("data", list.innerHTML)
+}
+
+
+/// to show todo tasks on screen
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
